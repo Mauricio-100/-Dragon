a4936728cd4d7913ddf69fccdf794eec278ea7e9
Mauricio-100 <ceoseshell@gmail.com>
2025-09-21 23:34:21 +0000

    feat: Mise en place de l'architecture monorepo et initialisation du Registre

--- packages/registry/index.js
+++ packages/registry/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require('express);
+const mysql = require('mysql2');
 const app = express();
 const PORT = 3000;
 
