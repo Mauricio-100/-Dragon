3345c5287239f08679ec8f6fa45dec7e7517939b
Mauricio-100 <ceoseshell@gmail.com>
2025-09-21 22:21:59 +0100

    Update index.js

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -5,7 +5,8 @@ const axios = require('axios');
 const program = new Command();
 
 // Définir l'URL de notre Registre
-const REGISTRY_URL = 'http://localhost:3000';
+// LIGNE CORRIGÉE
+const REGISTRY_URL = 'http://127.0.0.1:3000';
 
 program
   .name('drn')
